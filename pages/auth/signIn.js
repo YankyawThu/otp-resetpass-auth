f70315b62dc54639145eaf12fe8fed3591a97f14
Yan <yankyawthudev97@gmail.com>
2024-08-24 12:05:00 +0700

    add validation for auth

--- pages/auth/signIn.js
+++ pages/auth/signIn.js
@@ -42,16 +42,20 @@ export default function SignIn({csrfToken}) {
     }
 
     return (
-        <div>
-            <form method="post" onSubmit={handleSignIn}>
-                <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-                <input className="border-2 border-gray-500 bg-transparent p-1" value={email} type="email" onChange={e => setEmail(e.target.value)} name="email" placeholder="Enter email" />
-                {!email ? <span>Email is required</span> : <></>}
-                <input className="border-2 border-gray-500 bg-transparent p-1" value={password} type="password" onChange={e => setPassword(e.target.value)} name="password" placeholder="Enter password" />
-                <button type="submit">Sign in</button>
-            </form>
-            <button onClick={() => router.push('/auth/signUp')}>Register</button>
-            <button onClick={handleResetPass}>Reset password</button>
+        <div className="flex justify-center items-center h-screen">
+            <div>
+                <form method="post" onSubmit={handleSignIn} className="flex flex-col gap-1">
+                    <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+                    <input className="border-2 border-gray-500 bg-transparent p-1" value={email} type="email" onChange={e => setEmail(e.target.value)} name="email" placeholder="Enter email" />
+                    {!email ? <span>Email is required</span> : <></>}
+                    <input className="border-2 border-gray-500 bg-transparent p-1" value={password} type="password" onChange={e => setPassword(e.target.value)} name="password" placeholder="Enter password" />
+                    <button type="submit">Sign in</button>
+                </form>
+                <div className="flex flex-col my-3">
+                    <button onClick={handleResetPass}>Reset password</button>
+                    <button onClick={() => router.push('/auth/signUp')}>Register</button>
+                </div>
+            </div>
         </div>
     )
 }
--- pages/auth/signUp.js
+++ pages/auth/signUp.js
@@ -10,28 +10,36 @@ export default function SignUp() {
 
     const handleSignUp = async (e) => {
         e.preventDefault()
-        const response = await fetch('/api/auth/signUp', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ name, email, password }),
-        })
-        
-        if(response.ok) {
-            router.push('/auth/signIn')
+        if(email && password) {
+            const response = await fetch('/api/auth/signUp', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ name, email, password }),
+            })
+            
+            if(response.ok) {
+                router.push('/auth/signIn')
+            }
         }
     }
 
     return (
-        <div>
-            <form method="post" onSubmit={handleSignUp}>
-                <input className="border-2 border-gray-500 bg-transparent p-1" onChange={e => setName(e.target.value)} type="text" name="name" placeholder="Enter name" />
-                <input className="border-2 border-gray-500 bg-transparent p-1" onChange={e => setEmail(e.target.value)} type="email" name="email" placeholder="Enter email" />
-                <input className="border-2 border-gray-500 bg-transparent p-1" onChange={e => setPassword(e.target.value)} type="password" name="password" placeholder="Enter password" />
-                <button type="submit">Sign up</button>
-            </form>
-            <button onClick={() => router.push('/auth/signIn')}>Sign in</button>
+        <div className="flex justify-center items-center h-screen">
+            <div>
+                <form method="post" onSubmit={handleSignUp} className="flex flex-col gap-1">
+                    <input className="border-2 border-gray-500 bg-transparent p-1" onChange={e => setName(e.target.value)} type="text" name="name" placeholder="Enter name" />
+                    <input className="border-2 border-gray-500 bg-transparent p-1" onChange={e => setEmail(e.target.value)} type="email" name="email" placeholder="Enter email" />
+                    {!email ? <span>Email is required</span> : <></>}
+                    <input className="border-2 border-gray-500 bg-transparent p-1" onChange={e => setPassword(e.target.value)} type="password" name="password" placeholder="Enter password" />
+                    {!password ? <span>Password is required</span> : <></>}
+                    <button type="submit">Sign up</button>
+                </form>
+                <div className="flex flex-col my-3">
+                    <button onClick={() => router.push('/auth/signIn')}>Go Login</button>
+                </div>
+            </div>
         </div>
     )
 }
